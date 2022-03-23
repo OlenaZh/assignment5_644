c4274157e1c9eeacfef5b5dd20e868cc1b66ca0e
OlenaZh <alyona.mesh@gmail.com>
2022-03-23 00:10:17 -0700

    options added

--- js/script.js
+++ js/script.js
@@ -17,5 +17,21 @@ window.document.addEventListener("DOMContentLoaded", () => {
         console.log(`Extension: ${ext}`);
         console.log(`Email: ${email}`);
         console.log(`Department: ${department}`);
+
+        // NOT USING HELPER FUNCTION
+
+        /*console.log(`ID: ${document.getElementById('id').value}`);
+        console.log(`Name: ${document.getElementById('name').value}`);
+        console.log(`Extension: ${document.getElementById('ext').value}`);
+        console.log(`Email: ${document.getElementById('email').value}`);
+        console.log(`Department: ${document.getElementById('department').value}`);*/
+
+        // OR USE THE HELPER FUNCTION
+
+      /*console.log(`ID: ${$('id').value}`);
+        console.log(`Name: ${$('name').value}`);
+        console.log(`Extension: ${$('ext').value}`);
+        console.log(`Email: ${$('email').value}`);
+        console.log(`Department: ${$('department').value}`);*/
 });
 });
